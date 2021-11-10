cf4a06a9ad50ed46d579fb083d11aa52ab3d8d3e
Flaminia von Grolman <flaminia@vgrolman.de>
2021-11-10 16:05:54 +0000

    Restaurant & Review

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+5.times do
+  Restaurant.create(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.full_address,
+    phone_number: Faker::PhoneNumber.cell_phone,
+    category: ["chinese", "italian", "japanese", "french", "belgian"].sample
+  )
+end
+
+
+  Restaurant.create(name: "Flaminia", address: "London", phone_number: "012345567", category: "Chinese")
